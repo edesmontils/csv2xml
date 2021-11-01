392973ed24b7f15f9c266ec83c860fcdae10643a
edesmontils <emmanuel.desmontils@univ-nantes.fr>
2021-11-01 13:10:20 +0100

    Ajout des arguments

--- csv2xml.py
+++ csv2xml.py
@@ -6,27 +6,31 @@
 # - en ligne de commande définir l'id et ajouter l'attribut xml:id
 # - mettre en attribut les données "courtes" (comment les identifier ? est-ce que 1ere ligne suffit ?)
 # - gérer l'encodage (sans doute utf-8 par défaut)
-# - gérer les caractères &, ", ' dans mes cdc
+# - gérer les caractères &, ", ' dans les cdc
 # - si grand texte, section CDATA putôt que redéfinition des caractères dangereux.
 
 import csv
 import sys
 import os
 import glob
+import argparse
 
-# the optional command-line argument maybe a CSV file or a folder
-if len(sys.argv) == 2:
-    arg = sys.argv[1].lower()
-    if arg.endswith('.csv'): # if a CSV file then convert only that file
-        csvFiles = [arg]
-    else: # if a folder path then convert all CSV files in the that folder
-        os.chdir(arg)
-        csvFiles = glob.glob('*.csv')
-# if no command-line argument then convert all CSV files in the current folder
-elif len(sys.argv) == 1:
+
+parser = argparse.ArgumentParser(description='csv2xml converter')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-f", "--file", dest="csvFile", help="Nom du fichier CSV")
+group.add_argument("-d", "--dir", action="store_true", help="Tous les fichiers CSV du répertoire courant")
+
+args = parser.parse_args()
+
+if args.dir :
+    os.chdir('.')
     csvFiles = glob.glob('*.csv')
-else:
-    os._exit(1)
+else :
+    arg = args.csvFile
+    if arg.endswith('.csv'):
+        csvFiles = [arg]    
+
 
 
 for csvFileName in csvFiles:
